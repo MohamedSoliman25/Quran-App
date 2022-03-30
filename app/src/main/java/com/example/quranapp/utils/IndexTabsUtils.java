e75d3b77e6051e88da5c8c4762df9f339a7d6499
Mohamed Soliman <mohamedelzmar2018@gmail.com>
2022-03-30 23:24:59 +0200

    deleted unused files

--- app/src/main/java/com/example/quranapp/utils/IndexTabsUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.quranapp.utils;
-
-
-import com.example.quranapp.R;
-
-public class IndexTabsUtils {
-    public static final int [] QURAN_INDEX_TABS = {R.string.sora,R.string.jozz,R.string.page};
-    public enum QuranTabs {JOZZ,SORA,PAGE}
-}
